e32ea6e833e34b589a44c3922440d9dc9c95d6b1
Julian <0911707@hr.nl>
2017-01-06 17:05:59 +0100

    Add files via upload
    finished assignment 2

---
+++ Assignment/EntryPoint/KDTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryPoint{
+    interface ITree<Vector2>{
+        bool isEmpty { get; }
+        Vector2 value { get; }
+        bool evenForY { get; set; }
+        ITree<Vector2> left { get; }
+        ITree<Vector2> right { get; }
+    }
+
+    class Empty<Vector2> : ITree<Vector2>{
+        public bool evenForY { get; set; }
+
+        public bool isEmpty{
+            get{
+                return true;
+            }
+        }
+
+        public ITree<Vector2> left{
+            get{
+                throw new NotImplementedException();
+            }
+        }
+
+        public ITree<Vector2> right{
+            get{
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 value{
+            get{
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    class Node<Vector2> : ITree<Vector2>{
+        public bool evenForY{ get; set; }
+        public bool isEmpty{
+            get{
+                return false;
+            }
+        }
+
+        public ITree<Vector2> left { get; set; }
+
+        public ITree<Vector2> right { get; set; }
+
+        public Vector2 value { get; set; }
+
+        public Node(ITree<Vector2> l, Vector2 v, ITree<Vector2> r, bool x){
+            value = v;
+            left = l;
+            right = r;
+            evenForY = x;
+        }
+    }
+}
--- Assignment/EntryPoint/Program.cs
+++ Assignment/EntryPoint/Program.cs
@@ -3,40 +3,103 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace EntryPoint
-{
+namespace EntryPoint{
 #if WINDOWS || LINUX
-  public static class Program
-  {
+public static class Program{
+    static List<Vector2> tempList = new List<Vector2>();
+    static ITree<Vector2> insert(ITree<Vector2> t, Vector2 v, bool vert = true){
+        if (t.isEmpty){
+            return new Node<Vector2>(new Empty<Vector2>(), v, new Empty<Vector2>(), !vert);
+        }
+        if (t.value == v){
+            return t;
+        }
+
+        if (vert == true){
+            if (v.X < t.value.X){
+                return new Node<Vector2>(insert(t.left, v, !vert), t.value, t.right, !vert);
+            }
+            else{
+                return new Node<Vector2>(t.left, t.value, insert(t.right, v, !vert), !vert);
+            }
+        }
+        else{
+            if (v.Y < t.value.Y){
+                return new Node<Vector2>(insert(t.left, v, !vert), t.value, t.right, !vert);
+            }
+            else{
+                return new Node<Vector2>(t.left, t.value, insert(t.right, v, !vert), !vert);
+            }
+        }
+    }
+
+    static void searchInOrder(ITree<Vector2> t, double radius, Vector2 currentHouse){
+        if (!t.isEmpty) {
+            double distance = Math.Sqrt((Math.Pow((currentHouse.X - t.value.X), 2) + Math.Pow((currentHouse.Y - t.value.Y), 2)));
+
+            if (t.evenForY){ //if true, indexed based on y-axis
+                if (Math.Abs(currentHouse.Y - t.value.Y) < radius){
+                    if (distance <= radius){
+                        tempList.Add(t.value);
+                    }
+                    searchInOrder(t.left, radius, currentHouse);
+                    searchInOrder(t.right, radius, currentHouse);
+                    }
+                else if (t.value.Y > (currentHouse.Y + radius)){
+                    searchInOrder(t.left, radius, currentHouse);
+                }
+                else if (t.value.Y < (currentHouse.Y - radius)){
+                    searchInOrder(t.right, radius, currentHouse);
+                }
+            }
+            else{ //indexed based on x-axis
+                if (Math.Abs(currentHouse.X - t.value.X) < radius){
+                    if (distance <= radius){
+                        tempList.Add(t.value);
+                    }
+                    searchInOrder(t.left, radius, currentHouse);
+                    searchInOrder(t.right, radius, currentHouse);
+                }
+                else if (t.value.X > (currentHouse.X + radius)){
+                    searchInOrder(t.left, radius, currentHouse);
+                }
+                else if (t.value.X < (currentHouse.X - radius)){
+                    searchInOrder(t.right, radius, currentHouse);
+                }
+            }
+            //searchInOrder(t.left, radius, currentHouse);
+            //if (distance <= radius) {
+            //    tempList.Add(t.value);
+            //}
+            //searchInOrder(t.right, radius, currentHouse);
+        }
+    }
 
     [STAThread]
-    static void Main()
-    {
-
-      var fullscreen = false;
-      read_input:
-      switch (Microsoft.VisualBasic.Interaction.InputBox("Which assignment shall run next? (1, 2, 3, 4, or q for quit)", "Choose assignment", VirtualCity.GetInitialValue()))
-      {
-        case "1":
-          using (var game = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen))
-            game.Run();
-          break;
-        case "2":
-          using (var game = VirtualCity.RunAssignment2(FindSpecialBuildingsWithinDistanceFromHouse, fullscreen))
-            game.Run();
-          break;
-        case "3":
-          using (var game = VirtualCity.RunAssignment3(FindRoute, fullscreen))
-            game.Run();
-          break;
-        case "4":
-          using (var game = VirtualCity.RunAssignment4(FindRoutesToAll, fullscreen))
-            game.Run();
-          break;
-        case "q":
-          return;
-      }
-      goto read_input;
+    static void Main(){
+        var fullscreen = false;
+        read_input:
+        switch (Microsoft.VisualBasic.Interaction.InputBox("Which assignment shall run next? (1, 2, 3, 4, or q for quit)", "Choose assignment", VirtualCity.GetInitialValue())){
+            case "1":
+                using (var game = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen))
+                    game.Run();
+                break;
+            case "2":
+                using (var game = VirtualCity.RunAssignment2(FindSpecialBuildingsWithinDistanceFromHouse, fullscreen))
+                    game.Run();
+                break;
+            case "3":
+                using (var game = VirtualCity.RunAssignment3(FindRoute, fullscreen))
+                    game.Run();
+                break;
+            case "4":
+                using (var game = VirtualCity.RunAssignment4(FindRoutesToAll, fullscreen))
+                    game.Run();
+                break;
+            case "q":
+                return;
+        }
+        goto read_input;
     }
 
     private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings){
@@ -59,22 +122,42 @@ namespace EntryPoint
         foreach (Tuple<Vector2, double> x in BuildingsWithDistance) {
             finalList.Add(x.Item1);
         }
-
-        //return specialBuildings.OrderBy(v => Vector2.Distance(v, house));
         return finalList;
-        }
+        //return specialBuildings.OrderBy(v => Vector2.Distance(v, house));
+    }
 
     private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse(
-      IEnumerable<Vector2> specialBuildings, 
-      IEnumerable<Tuple<Vector2, float>> housesAndDistances)
-    {
-      return
-          from h in housesAndDistances
-          select
-            from s in specialBuildings
-            where Vector2.Distance(h.Item1, s) <= h.Item2
-            select s;
-    }
+        IEnumerable<Vector2> specialBuildings, 
+        IEnumerable<Tuple<Vector2, float>> housesAndDistances){
+        
+        //create 2d tree
+        var t = new Empty<Vector2>() as ITree<Vector2>;
+        foreach(Vector2 specialBuilding in specialBuildings){
+            t = insert(t, specialBuilding);
+        }
+
+        //find buildings in radius and add them in a list
+        List<List<Vector2>> combinedList = new List<List<Vector2>>();
+        foreach (Tuple<Vector2, float> DistanceAndHouse in housesAndDistances){
+            tempList.Clear();
+            double radius = DistanceAndHouse.Item2;
+            searchInOrder(t, radius, DistanceAndHouse.Item1);
+
+            List<Vector2> varList = new List<Vector2>();
+            foreach (Vector2 x in tempList){
+                varList.Add(x);
+            }
+            combinedList.Add(varList);
+        }
+        
+        return combinedList;
+        //return
+        //    from h in housesAndDistances
+        //    select
+        //    from s in specialBuildings
+        //    where Vector2.Distance(h.Item1, s) <= h.Item2
+        //    select s;
+        }
 
     private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding, 
       Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
@@ -109,6 +192,7 @@ namespace EntryPoint
       return result;
     }
     
+    //start merge sort
     private static void mergeSort(Tuple<Vector2, double>[] BuildingsWithDistance, int left, int right)
         {
             int mid;
@@ -152,7 +236,8 @@ namespace EntryPoint
             right--;
         }
     }
-
+    //end merge sort
+    
     }
 #endif
 }
